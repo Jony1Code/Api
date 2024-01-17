c8de685fbe0028c0fc3c472be6f6606fa883331b
ilya <ilya.zakora@yandex.ru>
2024-01-17 18:50:25 +0300

    feat: add_delete_update controller

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,16 +4,62 @@ exports.getAllTodo = async (req, res) => {
   try {
     const todos = await model.findAll();
     res.status(200).json(todos);
-  } catch {
+  } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
 exports.createTodo = async (req, res) => {
   try {
+    const { title = "", description = "" } = req.body;
+    if (!title || !description) {
+      throw new Error("Missing data");
+    }
     const todo = await model.create(req.body);
-    res.status(200).json(todo);
-  } catch {
+    res.status(201).json(todo);
+  } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title = "", description = "", isCompleted } = req.body;
+    const todo = await model.findByPk(id);
+    if (!todo) {
+      throw new Error("No task found");
+    }
+    await todo.update({ title, description, isCompleted });
+    res.status(200).json(todo);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getTodoById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await model.findOne({ where: { id } });
+    if (!todo) {
+      throw new Error("This task was not found");
+    }
+    res.status(200).json(todo);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+exports.deleteTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await model.findOne({ where: { id } });
+    if (!todo) {
+      throw new Error("Unexpected error");
+    }
+    await model.destroy({ where: { id } });
+    res.status(200).send("Success");
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllTodo, createTodo } = require("../controllers/controller");
+const {
+  getAllTodo,
+  createTodo,
+  updateTodo,
+  getTodoById,
+  deleteTodo,
+} = require("../controllers/controller");
 
+router.get("/:id", getTodoById);
 router.get("/", getAllTodo);
 router.post("/", createTodo);
+router.put("/:id", updateTodo);
+router.delete("/:id", deleteTodo);
 
 module.exports = router;
